10ac9c298d3d9f678ca8f13e872cfb02944a01ae
Jamie <kookjc6@gmail.com>
2020-03-27 22:39:48 -0500

    Updates

---
+++ precache-manifest.57cf1ba9f6670bce8709831fc070b2b6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3a82095acae8f731e87daf4461d0f5",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "14c3cb7cf32ab3cbac42",
+    "url": "/react-portfolio/static/css/main.1bb6e7d6.chunk.css"
+  },
+  {
+    "revision": "7f98dcf68058f4865424",
+    "url": "/react-portfolio/static/js/2.9b8f2cfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.9b8f2cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c3cb7cf32ab3cbac42",
+    "url": "/react-portfolio/static/js/main.4fb514c7.chunk.js"
+  },
+  {
+    "revision": "0346f5e54be923396aed",
+    "url": "/react-portfolio/static/js/runtime-main.8a7d631b.js"
+  },
+  {
+    "revision": "5fd11439ff1470858e6db2fe0b2ce654",
+    "url": "/react-portfolio/static/media/ResumeKook.5fd11439.pdf"
+  },
+  {
+    "revision": "5bbb44984cb2ab8f499a465b6661c77a",
+    "url": "/react-portfolio/static/media/collaboration.5bbb4498.jpg"
+  },
+  {
+    "revision": "d7ec9254f7931992998383fc137cddab",
+    "url": "/react-portfolio/static/media/data.d7ec9254.jpg"
+  },
+  {
+    "revision": "8486cc1baf4d423bb353036d55b0ceff",
+    "url": "/react-portfolio/static/media/forest.8486cc1b.jpg"
+  },
+  {
+    "revision": "1b55e57e3fecb826006575a8285ada46",
+    "url": "/react-portfolio/static/media/headshot.1b55e57e.jpg"
+  },
+  {
+    "revision": "cb9b5907260e0b164cea0e29b7c31a77",
+    "url": "/react-portfolio/static/media/linedpaper.cb9b5907.png"
+  },
+  {
+    "revision": "814922d9d0c9e6aa337b28b394b20069",
+    "url": "/react-portfolio/static/media/math.814922d9.jpg"
+  },
+  {
+    "revision": "8744ea0a86d96b30c74711b3be45b92e",
+    "url": "/react-portfolio/static/media/mountain.8744ea0a.jpg"
+  },
+  {
+    "revision": "a8215929e93a8ab107b06ae02fd4da9f",
+    "url": "/react-portfolio/static/media/organization.a8215929.jpg"
+  },
+  {
+    "revision": "a02a977d754c796f36870f336ab823d8",
+    "url": "/react-portfolio/static/media/portfolio.a02a977d.jpg"
+  },
+  {
+    "revision": "9988dc575c213042a311a7a554de8b5e",
+    "url": "/react-portfolio/static/media/present.9988dc57.jpg"
+  },
+  {
+    "revision": "21a3e1c2fc2881ace98c0213d39aa103",
+    "url": "/react-portfolio/static/media/software.21a3e1c2.jpg"
+  }
+]);
